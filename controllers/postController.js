cfc823af6c1da0409ced3ef9bd49dd5362f89243
Kenny Nguyen <sokenns@gmail.com>
2024-04-09 14:10:21 -0700

    Implement post list GET request

--- controllers/postController.js
+++ controllers/postController.js
@@ -1,4 +1,6 @@
 const Post = require("../models/post");
+const User = require("../models/user");
+const asyncHandler = require("express-async-handler");
 
 exports.post_get = (req, res, next) => {
   res.json("NOT IMPLEMENTED: Post GET");
@@ -12,9 +14,10 @@ exports.post_delete = (req, res, next) => {
   res.json("NOT IMPLEMENTED: Post DELETE");
 };
 
-exports.post_list = (req, res, next) => {
-  res.json("NOT IMPLEMENTED: Post list GET");
-};
+exports.post_list = asyncHandler(async (req, res, next) => {
+  const posts = await Post.find().sort({ timestamp: 1 }).populate("author").exec();
+  res.json(posts);
+});
 
 exports.post_post = (req, res, next) => {
   res.json("NOT IMPLEMENTED: Post POST");
