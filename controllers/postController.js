b5ed68716bf1688c619cdc7243e35d8a2650ddaf
Kenny Nguyen <sokenns@gmail.com>
2024-04-10 14:51:10 -0700

    Implement post GET and DELETE, and post list GET

--- controllers/postController.js
+++ controllers/postController.js
@@ -2,23 +2,60 @@ const Post = require("../models/post");
 const User = require("../models/user");
 const asyncHandler = require("express-async-handler");
 
-exports.post_get = (req, res, next) => {
-  res.json("NOT IMPLEMENTED: Post GET");
+exports.login_get = (req, res, next) => {
+  res.json("NOT IMPLEMENTED: Login POST.");
 };
+
+exports.login_post = (req, res, next) => {
+  res.json("NOT IMPLEMENTED: Login POST.");
+};
+
+exports.post_get = asyncHandler(async (req, res, next) => {
+  const post = await Post.findById(req.params.postid).exec();
+  if (post === null) {
+    // No results.
+    const err = new Error("Post not found.");
+    err.status = 404;
+    return next(err);
+  }
+  res.json(post);
+});
 
 exports.post_put = (req, res, next) => {
   res.json("NOT IMPLEMENTED: Post PUT");
 };
 
-exports.post_delete = (req, res, next) => {
-  res.json("NOT IMPLEMENTED: Post DELETE");
-};
+exports.post_delete = asyncHandler(async (req, res, next) => {
+  const post = await Post.findById(req.params.postid).exec();
+  if (post === null) {
+    // No results.
+    const err = new Error("Post not found.");
+    err.status = 404;
+    return next(err);
+  }
+  await post.deleteOne();
+  res.send("Post deleted.");
+});
 
 exports.post_list = asyncHandler(async (req, res, next) => {
-  const posts = await Post.find().sort({ timestamp: 1 }).populate("author").exec();
+  const posts = await Post.find()
+    .sort({ timestamp: 1 })
+    .populate("author")
+    .exec();
   res.json(posts);
 });
 
-exports.post_post = (req, res, next) => {
-  res.json("NOT IMPLEMENTED: Post POST");
-};
+exports.post_post = asyncHandler(async (req, res, next) => {
+  // const author = await User.findById(req.params.id).exec();
+
+  // const post = new Post({
+  //   author: author,
+  //   title: req.params.title,
+  //   text: req.params.text,
+  //   timestamp: req.params.timestamp,
+  //   published: req.params.published,
+  // });
+
+  // await post.save();
+  res.send("New post saved.");
+});
