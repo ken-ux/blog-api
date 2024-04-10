5be8dad0022f1047695798cb4f45c9a0a8146490
Kenny Nguyen <sokenns@gmail.com>
2024-04-09 17:56:11 -0700

    Add session and authentication config to app

--- app.js
+++ app.js
@@ -2,13 +2,16 @@ var express = require("express");
 var path = require("path");
 var cookieParser = require("cookie-parser");
 var logger = require("morgan");
+const mongoose = require("mongoose");
+const session = require("express-session");
+const MongoStore = require("connect-mongo");
+const passport = require("passport");
 
 var indexRouter = require("./routes/index");
 
 var app = express();
 
 // Set up mongoose connection
-const mongoose = require("mongoose");
 mongoose.set("strictQuery", false);
 const db_url = process.env.DATABASE_URL;
 const mongoDB = db_url;
@@ -24,6 +27,20 @@ app.use(express.json());
 app.use(express.urlencoded({ extended: false }));
 app.use(cookieParser());
 app.use(express.static(path.join(__dirname, "public")));
+
+// Set up session
+app.use(
+  session({
+    secret: process.env.SECRET,
+    resave: false,
+    saveUninitialized: true,
+    store: MongoStore.create({ mongoUrl: process.env.DATABASE_URL }),
+  })
+);
+
+// Set up authentication
+require("./config/passport");
+app.use(passport.session());
 
 app.use("/", indexRouter);
 
