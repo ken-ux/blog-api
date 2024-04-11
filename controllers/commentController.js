79323945eff4b690f6f5cab289b2a3c0bd00e3ea
Kenny Nguyen <sokenns@gmail.com>
2024-04-10 19:27:34 -0700

    Implement GET request for comments list

--- controllers/commentController.js
+++ controllers/commentController.js
@@ -1,4 +1,5 @@
 const Comment = require("../models/comment");
+const asyncHandler = require("express-async-handler");
 
 exports.comment_get = (req, res, next) => {
   res.json("NOT IMPLEMENTED: Comment GET");
@@ -8,9 +9,13 @@ exports.comment_delete = (req, res, next) => {
   res.json("NOT IMPLEMENTED: Comment DELETE");
 };
 
-exports.comment_list = (req, res, next) => {
-  res.json("NOT IMPLEMENTED: Comment list GET");
-};
+exports.comment_list = asyncHandler(async (req, res, next) => {
+  const comments = await Comment.find({ post: req.params.postid })
+    .sort({ timestamp: 1 })
+    .exec();
+
+  res.json(comments);
+});
 
 exports.comment_post = (req, res, next) => {
   res.json("NOT IMPLEMENTED: Comment POST");
