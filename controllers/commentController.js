ad90de706678c1390944a1ed999df60698dfc012
Kenny Nguyen <sokenns@gmail.com>
2024-04-10 21:49:22 -0700

    Implement comment deletion

--- controllers/commentController.js
+++ controllers/commentController.js
@@ -5,15 +5,23 @@ exports.comment_get = (req, res, next) => {
   res.json("NOT IMPLEMENTED: Comment GET");
 };
 
-exports.comment_delete = (req, res, next) => {
-  res.json("NOT IMPLEMENTED: Comment DELETE");
-};
+exports.comment_delete = asyncHandler(async (req, res, next) => {
+  const comment = await Comment.findById(req.params.commentid).exec();
+  if (comment === null) {
+    // No results.
+    const err = new Error("Comment not found.");
+    err.status = 404;
+    return next(err);
+  }
+
+  await comment.deleteOne();
+  res.send("Comment deleted.");
+});
 
 exports.comment_list = asyncHandler(async (req, res, next) => {
   const comments = await Comment.find({ post: req.params.postid })
     .sort({ timestamp: 1 })
     .exec();
-
   res.json(comments);
 });
 
