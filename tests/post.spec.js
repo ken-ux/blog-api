eae174debac3b125e592e5374ecf67f470f01caf
Kenny Nguyen <sokenns@gmail.com>
2024-04-27 02:45:41 -0700

    Add function for closing memory server

--- tests/post.spec.js
+++ tests/post.spec.js
@@ -1,7 +1,11 @@
 var express = require("express");
 var app = express();
+const mongoose = require("mongoose");
 const request = require("supertest");
-const initializeMongoServer = require("../config/mongoConfigTesting");
+const initializeMongoServer =
+  require("../config/mongoConfigTesting").initializeMongoServer;
+const closeMongoServer =
+  require("../config/mongoConfigTesting").closeMongoServer;
 var indexRouter = require("../routes/index");
 
 beforeAll(() => {
@@ -9,13 +13,15 @@ beforeAll(() => {
   app.use("/", indexRouter);
 });
 
-afterAll(async () => {});
+afterAll(() => {
+  closeMongoServer();
+});
 
 test("index route works", (done) => {
   request(app)
     .get("/posts")
     .expect("Content-Type", /json/)
-    .expect({ name: "frodo" })
+    .expect([])
     .expect(200)
     .end(function (err, res) {
       if (err) return done(err);
