2a0a91f567ca64edc3fc32383f1cfa936beb0779
Kenny Nguyen <sokenns@gmail.com>
2024-05-22 15:39:18 -0700

    Set-up JWT strategy in passport config

--- config/passport.js
+++ config/passport.js
@@ -1,5 +1,7 @@
 const passport = require("passport");
 const LocalStrategy = require("passport-local").Strategy;
+var JwtStrategy = require("passport-jwt").Strategy,
+  ExtractJwt = require("passport-jwt").ExtractJwt;
 const User = require("../models/user");
 const bcrypt = require("bcryptjs");
 
@@ -18,6 +20,24 @@ passport.use(
     } catch (err) {
       return done(err);
     }
+  })
+);
+
+const opts = {};
+opts.jwtFromRequest = ExtractJwt.fromAuthHeaderAsBearerToken();
+opts.secretOrKey = process.env.SECRET;
+passport.use(
+  new JwtStrategy(opts, function (jwt_payload, done) {
+    User.findOne({ id: jwt_payload.user }, function (err, user) {
+      if (err) {
+        return done(err, false);
+      }
+      if (user) {
+        return done(null, user);
+      } else {
+        return done(null, false);
+      }
+    });
   })
 );
 
